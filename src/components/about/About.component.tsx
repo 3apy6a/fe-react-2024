0b9e5979d162e18c93c18e122bf63890e7446b5b
Oleksandr Zaruba <oleksandr.zaruba@masterofcode.com>
2024-04-21 22:11:52 +0300

    added social network linc

--- src/components/about/About.component.tsx
+++ src/components/about/About.component.tsx
@@ -18,6 +18,10 @@ export const AboutComponent: React.FC<AboutProps> = ({ customClasses }) => (
             This is a social project from MOCG company where I got an opportunity to work with Frontend mentors and to create my own small
             project for the portfolio.
         </p>
-        <p>You can contact me via [social network name] and check out my GitHub.</p>
+        <p>
+            You can contact me via
+            <a href="https://www.instagram.com/3d_kuznya?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="> instagram</a> and
+            check out my <a href="https://github.com/3apy6a">GitHub</a>.
+        </p>
     </div>
 );
