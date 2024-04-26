3b25d3ccefc88da5b779ade38a0a099384513161
Oleksandr Zaruba <oleksandr.zaruba@masterofcode.com>
2024-04-26 23:30:06 +0300

    added shared components

---
+++ src/components/header/components/shared/icons/LightIcon.component.tsx
@@ -0,0 +1,20 @@
+import type { CustomClasses } from '@/interfaces/CustomClasses.ts';
+
+export const LightIconComponent = ({ customClasses }: CustomClasses) => (
+    <svg width="30" height="34" viewBox="0 0 30 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clipPath="url(#clip0_1373_3344)">
+            <path
+                d="M15 10.3334V8.66675M15 23.6667V25.3334M10.3452 12.3453L9.16666 11.1667M19.7733 21.7734L20.9519 22.952M8.33332 17.0001H6.66666M21.6667 17.0001H23.3333M19.7738 12.3453L20.9523 11.1667M10.3456 21.7734L9.16706 22.952M15 21.1667C12.6988 21.1667 10.8333 19.3013 10.8333 17.0001C10.8333 14.6989 12.6988 12.8334 15 12.8334C17.3012 12.8334 19.1667 14.6989 19.1667 17.0001C19.1667 19.3013 17.3012 21.1667 15 21.1667Z"
+                stroke="white"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </g>
+        <defs>
+            <clipPath id="clip0_1373_3344">
+                <rect width="20" height="20" fill="white" transform="translate(5 7)" />
+            </clipPath>
+        </defs>
+    </svg>
+);
