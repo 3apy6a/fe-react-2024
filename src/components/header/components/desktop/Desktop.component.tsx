989e110d19b64838578d6a0e89cb7505a0279d1e
Oleksandr Zaruba <oleksandr.zaruba@masterofcode.com>
2024-05-02 20:00:47 +0300

    refactored desktop component and styles

--- src/components/header/components/desktop/Desktop.component.tsx
+++ src/components/header/components/desktop/Desktop.component.tsx
@@ -1,5 +1,3 @@
-import classNames from 'classnames';
-
 import { CartComponent } from '@/components/header/components/shared/Cart.component.tsx';
 import { DarkIconComponent } from '@/components/header/components/shared/icons/DarktIcon.component.tsx';
 import { DividerIconComponent } from '@/components/header/components/shared/icons/DividerIcon.module.tsx';
@@ -9,65 +7,54 @@ import { SignUpIconComponent } from '@/components/header/components/shared/icons
 import { LogoComponent } from '@/components/header/components/shared/Logo.component.tsx';
 
 import styles from './desktop.module.css';
-export const DesktopComponent = () => {
-    const wrapperClasses = classNames({
-        [styles.flex]: true,
-        [styles.align_center]: true,
-    });
-
-    const linkClasses = classNames({
-        [styles.text_decoration]: true,
-        [styles.color_white]: true,
-    });
-    return (
-        <header className={styles.header}>
-            <div className={wrapperClasses}>
-                <LogoComponent />
-                <ul className={styles.theme}>
-                    <li>
-                        <button className={styles.theme}>
-                            <LightIconComponent customClasses={styles.offset_sm} />
-                        </button>
-                    </li>
-                    <li>
-                        <DividerIconComponent customClasses={styles.offset_sm} />
-                    </li>
-                    <li>
-                        <button className={styles.theme}>
-                            <DarkIconComponent customClasses={styles.offset_sm} />
-                        </button>
-                    </li>
-                </ul>
-            </div>
-            <div className={wrapperClasses}>
-                <ul className={styles.list}>
-                    <li>
-                        <a className={linkClasses} href="/about">
-                            About
-                        </a>
-                    </li>
-                    <li>
-                        <a className={linkClasses} href="/products">
-                            Products
-                        </a>
-                    </li>
-                    <li>
-                        <CartComponent />
-                    </li>
-                    <li className={styles.offset_md}>
-                        <a className={styles.login} href="/login">
-                            <LoginIconComponent customClasses={styles.offset_sm} />
-                            Login
-                        </a>
-                    </li>
-                    <li>
-                        <a className={styles.sign_up} href="/sign-up">
-                            <SignUpIconComponent customClasses={styles.offset_sm} />
-                            Sign up
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </header>
-    );
-};
+export const DesktopComponent = () => (
+    <header className={styles.header}>
+        <div className={`${styles.flex} ${styles.align_center}`}>
+            <LogoComponent />
+            <ul className={styles.theme}>
+                <li>
+                    <button className={styles.theme}>
+                        <LightIconComponent customClasses={styles.offset_sm} />
+                    </button>
+                </li>
+                <li>
+                    <DividerIconComponent customClasses={styles.offset_sm} />
+                </li>
+                <li>
+                    <button className={styles.theme}>
+                        <DarkIconComponent customClasses={styles.offset_sm} />
+                    </button>
+                </li>
+            </ul>
+        </div>
+        <div className={`${styles.flex} ${styles.align_center}`}>
+            <ul className={styles.list}>
+                <li>
+                    <a className={`${styles.text_decoration} ${styles.color_white}`} href="/about">
+                        About
+                    </a>
+                </li>
+                <li>
+                    <a className={`${styles.text_decoration} ${styles.color_white}`} href="/products">
+                        Products
+                    </a>
+                </li>
+                <li>
+                    <CartComponent />
+                </li>
+                <li className={styles.offset_md}>
+                    <a className={styles.login} href="/login">
+                        <LoginIconComponent customClasses={styles.offset_sm} />
+                        Login
+                    </a>
+                </li>
+                <li>
+                    <a className={styles.sign_up} href="/sign-up">
+                        <SignUpIconComponent customClasses={styles.offset_sm} />
+                        Sign up
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </header>
+);
